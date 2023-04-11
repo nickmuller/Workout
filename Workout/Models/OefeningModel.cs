b4e3a0316cb62a6d0c4a52bb2977aaaf89e2bc3e
Nick Muller <nick.muller@robidus.nl>
2023-04-11 22:46:18 +0200

    Alle oefeningen toegevoegd

--- Workout/Models/OefeningModel.cs
+++ Workout/Models/OefeningModel.cs
@@ -7,6 +7,7 @@ public readonly record struct OefeningModel
     string Naam,
     int AantalSets,
     int AantalHerhalingen,
+    TimeSpan DuurSet,
     TimeSpan DuurPauze,
     string[] Tips,
     string AfbeeldingUrl,
