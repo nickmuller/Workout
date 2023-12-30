08c54c1d74d716f93d52ef20939c033bf77b5818
Nick <nickmuller@gmail.com>
2023-12-30 23:28:26 +0100

    Refactor readonly record struct met defaults

--- Workout/Models/OefeningModel.cs
+++ Workout/Models/OefeningModel.cs
@@ -2,16 +2,15 @@
 
 namespace Workout.Models;
 
-public readonly record struct OefeningModel(
-    string Naam,
-    int AantalSets,
-    int AantalHerhalingen,
-    string AfbeeldingUrl,
-    string VideoUrl
-)
+public readonly record struct OefeningModel()
 {
+    public required string Naam { get; init; }
+    public required int AantalSets { get; init; }
+    public required int AantalHerhalingen { get; init; }
     public string[] Tips { get; init; } = Array.Empty<string>();
     public TimeSpan DuurSet { get; init; } = TimeSpan.FromMinutes(1);
     public TimeSpan DuurPauze { get; init; } = TimeSpan.FromSeconds(40);
+    public string? AfbeeldingUrl { get; init; }
+    public string? VideoUrl { get; init; }
     public InitieelTonen InitieelTonen { get; init; } = InitieelTonen.Afbeelding;
 }
